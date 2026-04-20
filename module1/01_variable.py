550aba33f4b540e5f669fc96d62194e06d50fc66
NamSangKyu <nam2626@naver.com>
2026-04-20 10:27:35 +0900

    변수 및 출력

---
+++ module1/01_variable.py
@@ -0,0 +1,14 @@
+name = '홍길동'
+age = 20
+is_student = True
+score = None
+
+print(name,age,is_student,score)
+
+# type()으로 변수 타입을 확인 가능
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(score))
+
+# -- 문자열 기본적인 조작 --
