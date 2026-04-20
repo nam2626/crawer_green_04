4f6282175ea1085290043cac1c7c192f4f21614a
NamSangKyu <nam2626@naver.com>
2026-04-20 10:58:26 +0900

    Add additional string manipulation examples in 01_variable.py

--- module1/01_variable.py
+++ module1/01_variable.py
@@ -19,3 +19,14 @@ print(greetring.strip().upper()) # 알파벳 대문자 변환
 print(greetring.strip().lower()) # 알파벳 소문자 변환
 print(len(greetring)) # 문자열 길이(공백 포함)
 print(len(greetring.strip()))
+
+url = 'https://example.com/board?page=1'
+print(url.split('?')) # ?를 기준으로 분리 -> 리스트로 반환
+print(url.split('?')[1])
+arr = url.split('?')
+print(arr[len(arr)-1])
+print(url.replace('https','http')) # https -> http로 변환
+
+# in을 이용해서 특정 문자열 포함 여부를 확인 가능
+print('example' in url)
+print('example1' in url)
